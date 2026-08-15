86d650a17ee9807830879b4ddba6ce61e6b34b47
mayurgarud <mayurgarud06@gmail.com>
2026-08-15 14:27:58 +0530

    testing time changes

--- backend/src/main/java/com/networkstream/api/NetworkStreamApplication.java
+++ backend/src/main/java/com/networkstream/api/NetworkStreamApplication.java
@@ -2,8 +2,12 @@ package com.networkstream.api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = "com.networkstream")
+@EnableJpaRepositories(basePackages = "com.networkstream.repository")
+@EntityScan(basePackages = "com.networkstream.domain")
 public class NetworkStreamApplication {
 
     public static void main(String[] args) {
